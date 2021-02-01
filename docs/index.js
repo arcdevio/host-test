42b57d12b1467065993a16df119d930aff8c2bee
Alexander Elias <alex.steven.elias@gmail.com>
2021-02-01 14:50:57 -0800

    added log

--- docs/index.js
+++ docs/index.js
@@ -5,11 +5,15 @@ form.addEventListener('submit', async function (e) {
 
     const body = Object.fromEntries(new FormData(e.target));
 
+    console.log(body);
+
     const result = await window.fetch(body.$url, {
         mode: 'cors',
         method: 'POST',
         body: JSON.stringify(body)
     });
+
+    console.log(await result.text());
 
     const { status } = result;
     const { message } = await result.json();
